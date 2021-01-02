c2efa8001a012d6b43917281ef61d73255695452
sviat-h <svyatoslav.grinik@gmail.com>
2021-01-02 13:11:36 +0200

    updated .yml config and disable default spring security login page

---
+++ src/main/java/com/onlinegroceryshopping/demo/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.onlinegroceryshopping.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //disable default login page
+        http.httpBasic().disable();
+    }
+}
