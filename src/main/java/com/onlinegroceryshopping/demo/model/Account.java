4cd4348f60369c1ce2a37e7166fc688445341625
sviat-h <svyatoslav.grinik@gmail.com>
2021-01-02 13:09:42 +0200

    added Account entity

---
+++ src/main/java/com/onlinegroceryshopping/demo/model/Account.java
@@ -0,0 +1,72 @@
+package com.onlinegroceryshopping.demo.model;
+
+import com.onlinegroceryshopping.demo.model.enums.Role;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "accounts")
+public class Account implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "password")
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+
+    @Column(name = "activation_code")
+    private String activationCode;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive();
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+}
