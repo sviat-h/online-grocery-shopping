a5b1102b0363241695d9b05bf81884ff69992f7b
sviat <svyatoslav.grinik@gmail.com>
2021-10-17 00:29:25 +0300

    //

--- src/main/java/com/onlinegroceryshopping/demo/OnlineGroceryShoppingApplication.java
+++ src/main/java/com/onlinegroceryshopping/demo/OnlineGroceryShoppingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineGroceryShoppingApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(OnlineGroceryShoppingApplication.class, args);
     }
